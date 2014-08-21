5fa7c10cf3a4f712b50866cd7b71adc4f41ff1a0
stilez1 <otto.perakyla@gmail.com>
2014-08-21 15:41:28 +0300

    Update htmlEmailTester.js

--- htmlEmailTester.js
+++ htmlEmailTester.js
@@ -1,62 +1,45 @@
 (function(){
+	'use strict';
 
 // todo:
 // BACKGROUND CENTERING
-// check for single line cells with line-height differing from font-size
 
-var db = document.body;
 var defBg = 'bg.jpg';
+var db = document.body;
 var defOutline = "1px solid #000";
 var bgVal = getBgUrl(defBg);
+var bodyHeight = document.body.clientHeight;
 
 db.style.backgroundImage = bgVal;
+db.style.height = 200 + bodyHeight + "px";
 db.style.backgroundRepeat    = "no-repeat";
-db.style.backgroundPositionX = "505px";//...
-db.style.backgroundPositionY = "0px";
 
-/*
-need to init image with current document background and get its width
-then set backgroundPositionX to (document.clientWidth-backgroundImage.width)/2 (IN PIXELS!)
-var imgg = new Image();
-imgg.src = getCurrentBg();	
-console.log(getCurrentBg());
-cant get image/image size like this for some reason (not loaded yet??)
-console.log(imgg);
-*/
+db.style.backgroundPositionX = localStorage.getItem('backgroundX') ? localStorage.getItem('backgroundX') : calcDefaultBgPos() + "px";//default to mid
+db.style.backgroundPositionY = localStorage.getItem('backgroundY') ? localStorage.getItem('backgroundY') : "0px";
 
+/* HTML */
 var html = "";
 html += "<div id='hideContainer'>";
-html += "<span>[<a id='hideTester' href='#'>hide tester</a>]</span>";
+html += "<span>[<a id='toggleTester' href='#'>hide tester</a>]</span>";
 html += "</div>";
 html += "<div id='htmlTester'>";
 html += "<h1>Html email tester</h1>";
-html += "<label for='url'>Bg url:</label><br />";
-html += "<input style='background-color:#0f0;' type='text' name='url' id='url' value='bg.jpg'></input><br />";
-html += "<button id='up'>&#8593;</button>";
-html += "<button id='down'>&#8595;</button>"; 
+html += "<input style='margin-left:0;' type='checkbox' id='isEemeliMail'></input><label for='isEemeliMail'>Eemeli mail</label><br />";
+html += "<button style='width:100%' id='resetBg'>Reset bg</button>";
 html += "<br />";
-html += "<button id='left'>&#8592;</button>"; 
-html += "<button id='right'>&#8594;</button>"; 
+html += "<button style='width:100%' id='toggleBg'>Hide bg</button>";
 html += "<br />";
-html += "<button id='reset'>Reset bg</button>";
+html += "<button style='width:100%' id='toggleHtml'>Hide html</button>";
 html += "<br />";
-html += "<button id='hide'>Hide bg</button>";
+html += "<button style='width:100%' id='toggleBorders'>Show borders</button>";
 html += "<br />";
-html += "<button id='html'>Hide html</button>";
-html += "<br />";
-html += "<button id='borders'>Show borders</button>";
-html += "<br />";
-html += "<button id='check'>Check html</button>";
-html += "<h3>These cells will break in Outlook</span></h3>";
-html += "<p style='background-color:#f00;border:1px solid #000;'>Cell is too low in height</p>";
-html += "<h3>These cells could cause problems in Outlook</span></h3>";
-html += "<p style='background-color:#00f;border:1px solid #000;color:#fff'>Cell is missing valign or align-attribute</p>";
-html += "<h3>Unsemantic markup</span></h3>";
-html += "<p style='background-color:hotpink;border:1px solid #000;color:#fff'>Image is missing alt attribute or has empty alt text (alt=\"\")</p>";
-html += "</div>"
+html += "<button style='width:100%' id='checkHtml'>Check html</button>";
+html += "</div>";
 
 var formDiv = document.createElement("div");
 var css = {
+	fontFamily: 'Comic Sans MS',
+	color: 'hotpink',
 	position: 'fixed',
 	top: '20px',
 	left: '20px',
@@ -65,143 +48,144 @@ var css = {
 	backgroundColor: "#fff"
 };
 
-for(var rule in css)
-	formDiv.style[rule] = css[rule];
+setStyles(formDiv, css);
 
 formDiv.setAttribute("id", "container");
 formDiv.innerHTML = html;
 
 db.insertBefore(formDiv, db.childNodes[0]);
+/* END HTML */
 
-var upBtn    = document.getElementById("up");
-var downBtn  = document.getElementById("down");
-var leftBtn  = document.getElementById("left");
-var rightBtn  = document.getElementById("right");
-var hideBtn  = document.getElementById("hide");
-var checkBtn = document.getElementById("check");
-var resetBtn = document.getElementById("reset");
-var urlBox   = document.getElementById("url");
-var hideTesterBtn = document.getElementById("hideTester");
-var borderBtn = document.getElementById("borders");
-var htmlBtn = document.getElementById("html");
+var bgPosY = parseInt(db.style.backgroundPositionY);
+var bgPosX = parseInt(db.style.backgroundPositionX);
+
+document.onkeypress = function(event) {
+	event = event || window.event;
+	var charCode = (typeof event.which == "number") ? event.which : event.keyCode;
+
+	if(charCode) {
+		if(charCode == 13) { // enter pressed, save position
+			localStorage.setItem('backgroundX', db.style.backgroundPositionX);
+			localStorage.setItem('backgroundY', db.style.backgroundPositionY);
+			alert("Bg position saved");
+		} else {
+			var keyPressed = String.fromCharCode(charCode).toLowerCase();
+			switch(keyPressed) {
+				case 'w':
+				db.style.backgroundPositionY = --bgPosY + "px";
+				break;
+
+				case 's':
+				db.style.backgroundPositionY = ++bgPosY + "px";
+				break;
+
+				case 'a':
+				db.style.backgroundPositionX = --bgPosX + "px";
+				break;
+
+				case 'd':
+				db.style.backgroundPositionX = ++bgPosX + "px";
+				break;
+
+				default:
+				break;
+			}
+		}
+	}
+};
+
+var loc = window.location.href;
+
+var isEemeliMail = document.getElementById("isEemeliMail");
+
+if (localStorage.getItem('eemeli')) {
+	if (localStorage.eemeli) {
+		isEemeliMail.checked = true;
+	} else {
+		isEemeliMail.checked = false;
+	}
+}
 
 var testerHidden = false;
 var bgHidden = false;
 var htmlHidden = false;
 var bordersOn = false;
 
-var bgPosY = parseInt(db.style.backgroundPositionY);
-var bgPosX = parseInt(db.style.backgroundPositionX);
-var borderBtnWidth = borderBtn.offsetWidth;
-var allBtns = document.getElementsByTagName("button");
-var widestBtnLen = allBtns[0].clientWidth;
+/* Event listeners */
+isEemeliMail.addEventListener('click', function(){
+	localStorage.setItem('eemeli', this.checked ? true : '');
+},false);
 
-for (var i = 1; i < allBtns.length; i++) {
-	if (allBtns[i].clientWidth > widestBtnLen) {
-		widestBtnLen = allBtns[i].clientWidth;
-	} 
-}
-
-for (var i = 0; i < allBtns.length; i++) {
-	console.log("setting", allBtns[i], "to", widestBtnLen + "px");
-	allBtns[i].style.width = widestBtnLen + "px";
-}
-
-hideBtn.addEventListener('click', function(){
+document.getElementById("toggleBg").addEventListener('click', function(){
 	if (bgHidden) {
 		db.style.backgroundImage = bgVal;
 		bgHidden = false;
-		hideBtn.innerText = "Hide bg";
+		this.innerText = "Hide bg";
 	} else {
 		db.style.backgroundImage = "";
 		bgHidden = true;
-		hideBtn.innerText = "Show bg";
+		this.innerText = "Show bg";
 	}
 }, false);
 
-hideTesterBtn.addEventListener('click', function(){
+document.getElementById("toggleTester").addEventListener('click', function(){
 	if (testerHidden) {
 		testerHidden = false;
-		hideTesterBtn.innerText = "hide tester";
+		this.innerText = "hide tester";
 		document.getElementById("htmlTester").style.display="block";
 	} else {
 		testerHidden = true;
-		hideTesterBtn.innerText = "show tester";
+		this.innerText = "show tester";
 		document.getElementById("htmlTester").style.display="none";
 	}
 }, false);
 
-htmlBtn.addEventListener('click', function(){
+document.getElementById("toggleHtml").addEventListener('click', function(){
 	if(htmlHidden){
 		htmlHidden = false;
-		htmlBtn.innerText = "Hide html";
+		this.innerText = "Hide html";
 		document.getElementsByTagName("table")[0].style.display = "table";
 	} else {
 		htmlHidden = true;
-		htmlBtn.innerText = "Show html";
+		this.innerText = "Show html";
 		document.getElementsByTagName("table")[0].style.display = "none";
 	}
 },false);
 
-borderBtn.addEventListener('click', function(){
+document.getElementById("toggleBorders").addEventListener('click', function(){
 	if (bordersOn) {
 		bordersOn = false;
-		borderBtn.innerText = "Show borders";
+		this.innerText = "Show borders";
 		setBorders(0);
 	} else {
 		bordersOn = true;
-		borderBtn.innerText = "Hide borders";
+		this.innerText = "Hide borders";
 		setBorders(1);
 	}
 }, false);
 
-upBtn.addEventListener('click', function(){
-	db.style.backgroundPositionY = --bgPosY + "px";
-}, false);
-
-downBtn.addEventListener('click', function(){
-	db.style.backgroundPositionY = ++bgPosY + "px";
-}, false);
-
-leftBtn.addEventListener('click', function(){
-	db.style.backgroundPositionX = --bgPosX + "px";
-}, false);
-
-rightBtn.addEventListener('click', function(){
-	db.style.backgroundPositionX = ++bgPosX + "px";
-}, false);
-
-checkBtn.addEventListener('click', function(){
+document.getElementById("checkHtml").addEventListener('click', function(){
 	checkHTML();
 }, false);
 
-resetBtn.addEventListener('click', function(){
+document.getElementById("resetBg").addEventListener('click', function(){
+	var defaultBgX = calcDefaultBgPos() + "px";
 	db.style.backgroundPositionY = "0px";
+	db.style.backgroundPositionX = defaultBgX;
+	localStorage.setItem('backgroundY', "0px");
+	localStorage.setItem('backgroundX', defaultBgX);
 }, false);
 
-urlBox.addEventListener('change', function(){
-	testImage(urlBox.value);
-}, false);
+/* End event listeners */
 
-// helper functions
-function testImage(URL) {
-	var tester=new Image();
-	tester.onload=imageFound;
-	tester.onerror=imageNotFound;
-	tester.src=URL;
-	console.log(URL);
+/* Functions */ 
+function calcDefaultBgPos(){
+	return 520;
 }
 
-function imageFound() {
-	db.style.backgroundImage = getBgUrl(urlBox.value);
-	urlBox.style.backgroundColor = "#0f0";
-	alert(getImgMsg() + " loaded as background");
-}
-
-function imageNotFound() {
-	db.style.backgroundImage = "";
-	urlBox.style.backgroundColor = "#f00";
-	alert(getImgMsg() + " cannot be found");
+function setStyles(el, css){
+	for(var rule in css)
+		el.style[rule] = css[rule];
 }
 
 function setBorders(bool){
@@ -232,20 +216,39 @@ function getOutline(bool){
 		return bool + "px solid #000";
 	}
 
-	return "none";
+	return "";
 }
 
 function getCurrentBg(){
-	return db.style.backgroundImage.match(/url\(([^\)]+)/)[1];;
-}
-
-function getImgMsg(){
-	return "Image with src: '" + urlBox.value;
+	return db.style.backgroundImage.match(/url\(([^\)]+)/)[1];
 }
 
 function getBgUrl(bgUrl){
 	return "url('"+bgUrl+"')";
 }
+
+function attachErrorMsgListeners() {
+	var errorTdLis = document.getElementById("errors").getElementsByTagName("li");
+
+	for (var i = 0; i < errorTdLis.length; i++) {
+		errorTdLis[i].addEventListener("mouseover", function(event){
+			var css = {
+				outline: "1px solid #f00"
+			};
+			var domElement = document.getElementById("dom"+event.target.id);
+			var orgHeight = domElement.offsetHeight;
+			setStyles(domElement,css);
+			
+		}, false);
+		errorTdLis[i].addEventListener("mouseout", function(event){
+			setTimeout(function(){
+				document.getElementById("dom"+event.target.id).style.outline="none";
+			}, 500);
+		})
+	};
+
+}
+
 
 function checkHTML(){
 	var table = document.getElementsByTagName("table")[0];
@@ -257,10 +260,11 @@ function checkHTML(){
 	var img = "";
 	var a   = "";
 
-	var emptyAlts     = [];
-	var missingAlts   = [];
-	var heightTooLow  = [];
-	var missingSnoobi = [];
+	var emptyAlts    	 = [];
+	var missingAlts  	 = [];
+	var heightTooLow 	 = [];
+	var missingSnoobi    = [];
+	var missingRedirect  = [];
 
 	var errors = false;
 	var warnings = false;
@@ -286,61 +290,126 @@ function checkHTML(){
 		return el.innerText.length ? el.innerText : false;
 	}
 
+	function getTesterOffset() {
+		var c = document.getElementById("container");
+		return c.offsetHeight + c.offsetTop;
+	}
+
+	function getErrorLink(el) {
+		// strip "dom" from element id
+		return "<li><a id='" + el.id.substring(3) + "' style='color:#00f;' href='#'>" + el + "</a></li>";
+	}
+
+	function printMessageForm(message) {
+		var formDiv = document.createElement("div");
+		var css = {
+			position: 'fixed',
+			top: "20px",
+			width: document.getElementById("container").offsetWidth + "px",
+			right: '20px',
+			border: '1px solid #000',
+			padding: '20px',
+			backgroundColor: "#fff",
+			overflow: "auto"
+		};
+
+		setStyles(formDiv, css);
+
+		formDiv.setAttribute("id", "errors");
+
+		formDiv.innerHTML = message;
+		db.insertBefore(formDiv, db.childNodes[0]);
+		attachErrorMsgListeners();
+	}
+
 	// check tds
 	var hasHeight = false;
+
 	for(var i=0;i<tds.length;i++){
 		td = tds[i];
 		hasHeight = td.hasAttribute("height");
 		if((td.offsetHeight < 19 && !hasHeight)){
-			td.style.outline = defOutline; // using outline because it doesnt increase element height/width
-			td.style.backgroundColor="#f00";
+			//td.style.outline = "1px solid #f00"; // using outline because it doesnt increase element height/width
+			//td.style.backgroundColor="#f00";
+			td.setAttribute('id','domErrorTd_'+i);
+			console.log(td);
+			heightTooLow.push(td);
 			setError();
 		} 
 
 		if (!td.hasAttribute("valign") || !td.hasAttribute("valign")) {
-			td.style.outline = defOutline;
-			td.style.backgroundColor = "#00f";
-			setError();
-		}
-	}
+			//td.style.outline = defOutline;
+	//		td.style.backgroundColor = "#00f";
+	setError();
+}
+}
 
 	// check imgs
 	for (var i = 0; i < imgs.length; i++) {
 		img = imgs[i];
 		if (!img.hasAttribute("alt") || img.getAttribute("alt") == "") {
-			img.style.outline = "1px solid hotpink";
-			setError();
-		} 
-	}
+	//		img.style.outline = "1px solid hotpink";
+	setError();
+} 
+}
 
 	// check as
 	for (var i = 0; i < as.length; i++) {
-		href = as[i].href;
-		if (ignoreLinkRegex.test(href)) 
+		if (ignoreLinkRegex.test(as[i])) 
 			continue;
 
-		if (!snoobiRegex.test(href)) {
-			missingSnoobi.push(href);
+		if (!snoobiRegex.test(as[i])) {
+			missingSnoobi.push(as[i]);
 			setError();
 		}
 
+		if (isEemeliMail.checked && !as[i].hasAttribute("redirect")) {
+			as[i].setAttribute('id', 'domErrorRedirect_'+i);
+			missingRedirect.push(as[i]);
+			setError();
+		}
 	}
 
-	if(missingSnoobi.length) {
-		var missingSnoobiString = "";
-		var errorMsg = "";
+	if(errors) {
+		var missingSnoobiString = "<ul id='snoobiErrors'>";
+		var heightTooLowString = "<ul id='heightTooLowErrors'>";
+		var missingRedirectString = "<ul id='missingRedirectErrors'>";
+		var errorMsg = "<h2 style='color:#f00 !important;'>Errors</h2>";
 
 		for (var i = 0; i < missingSnoobi.length; i++) {
-			missingSnoobiString += missingSnoobi[i] + "\n";
+			missingSnoobiString += "<li>" + missingSnoobi[i] + "</li>";
 		}
+		missingSnoobiString += "</ul>";
+
+		for (var i = 0; i < heightTooLow.length; i++) {
+			heightTooLowString += getErrorLink(heightTooLow[i]);
+		}
+		heightTooLowString += "</ul>";
+
+		for (var i = 0; i < missingRedirect.length; i++) {
+			missingRedirectString +=  getErrorLink(missingRedirect[i]);
+		}
+		missingRedirectString += "</ul>";
 
 		if (missingSnoobiString) {
-			errorMsg += "Found links with missing/broken snoobi tags:\n" + missingSnoobiString;
+			errorMsg += "<h3>Found links with missing/broken snoobi tags</h3>" + missingSnoobiString;
 		}
 
-		alert(errorMsg);
+		if (heightTooLowString) {
+			errorMsg += "<h3>Found tds with too low height</h3>" + heightTooLowString;
+		}
+
+		if (missingRedirect.length && missingRedirectString) {
+			errorMsg += "<h3>Found hrefs with missing redirect-attribute (eemeli-mail)</h3>" + missingRedirectString;
+		}
+
+		printMessageForm(errorMsg);
 	} else if(!errors) { 
-		alert("HTML passed! ;-)");
+		var html = "";
+		html += "<h2 style='font-family:Comic Sans MS;color:hotpink !important;'>Html passed ;-)</h2>";
+		html += "<img style='margin:20px 0;' src='http://i.imgur.com/igEwpy7.jpg' />";
+		html += "<iframe width='320' height='315' src='//www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1' frameborder='0' allowfullscreen></iframe>";
+		printMessageForm(html);
 	}
 }
 
